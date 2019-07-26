453beac8b218b712503527b7d09242d754cb4b00
Andrew Obiano <andrew.obiano@gmail.com>
2019-07-25 18:04:11 -1000

    Finished

--- smallest_multiple.js
+++ smallest_multiple.js
@@ -6,9 +6,21 @@
  * @return { Number }         Lowest Positive Number that is evenly divisible by all numbers
  *                            between 1 and `ceiling`
  */
-module.exports = function( ceiling ) {
+module.exports = function(ceiling) {
   // do work here
+  let smallestNumber = 1;
+  let temp = 1;
 
+  while (temp <= ceiling) {
+    // cut the loop if ceiling is reached
+    if (smallestNumber % temp === 0) {
+      // check each temp multiple
+      temp++; // iterate through the temp numbers
+    } else {
+      smallestNumber++;
+      temp = 1; // start the reiteration again
+    }
+  }
 
-  return 0;
+  return smallestNumber;
 };
